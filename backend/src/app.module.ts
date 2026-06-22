600cc5ebfd0661884a5b001b880cc0d5e12971eb
achilles <ws1935440@gmail.com>
2026-06-22 02:23:35 +0100

    fixed deployment error

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { AppConfigModule } from './config/app-config.module'
-import { CacheModule } from './cache/cache.module'
-import { SportsModule } from './sports/sports.module'
-
-@Module({
-  imports: [AppConfigModule, CacheModule, SportsModule],
-})
-export class AppModule {}
--- backend/src/cache/cache.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common'
-import { MemoryCacheService } from './memory-cache.service'
-
-@Global()
-@Module({
-  providers: [MemoryCacheService],
-  exports: [MemoryCacheService],
-})
-export class CacheModule {}
